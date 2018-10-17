ad31f03bd4c0a56912a6bc844c89e3b55c5a339c
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2018-10-17 14:08:36 +0530

    First, last name, password and mo no fields are added

---
+++ Bazar2/app/src/main/java/com/example/glondhe/bazar/greetActivity.java
@@ -0,0 +1,13 @@
+package com.example.glondhe.bazar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class greetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greet);
+    }
+}
